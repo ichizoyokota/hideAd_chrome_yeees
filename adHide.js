22e640e13c7cfe00db6b314fa6d545323bf2d207
ichizoyokota <yokota@yeees.in>
2024-01-20 13:50:49 +0900

    広告回避ボタンクリックからリロードへ変更

---
+++ adHide.js
@@ -0,0 +1,24 @@
+let observer1 = new MutationObserver((m) => {
+    m.forEach((r) => {
+        if (r.target.className === "ytp-ad-skip-button-container ytp-ad-skip-button-container-detached") {
+            let tmp = document.getElementsByClassName("ytp-ad-skip-button-container ytp-ad-skip-button-container-detached");
+            if (tmp.length > 0) {
+                console.log(tmp[0])
+                location.reload()
+            }
+        }
+    })
+});
+
+
+let target = document.getElementsByTagName('body');
+
+if (target[0] !== undefined) {
+    observer1.observe(target[0], {
+        childList: true,
+        subtree: true,
+        attributes: true,
+        attributeFilter: ['style', 'className']
+    });
+}
+
