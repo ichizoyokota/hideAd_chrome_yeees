995df819e075a7675b9ee2710741d8648398c527
ichizoyokota <yokota@yeees.in>
2024-04-21 07:34:54 +0900

    Youtubeのスキップボタン：DisneyのCMでは仕様が変わっていたので対応を追加。

--- youtube.js
+++ youtube.js
@@ -9,12 +9,29 @@ let observer1 = new MutationObserver((m) => {
         }
     })
 });
+let observer2 = new MutationObserver((m) => {
+    m.forEach((r) => {
+        if (r.target.className === "ytp-skip-ad-button") {
+            let tmp = document.getElementsByClassName("ytp-skip-ad-button");
+            if (tmp.length > 0) {
+                // console.log(tmp[0])
+                tmp[0].click();
+            }
+        }
+    })
+});
 
 
 let target = document.getElementsByTagName('body');
 
 if (target[0] !== undefined) {
     observer1.observe(target[0], {
+        childList: true,
+        subtree: true,
+        attributes: true,
+        attributeFilter: ['style', 'className']
+    });
+    observer2.observe(target[0], {
         childList: true,
         subtree: true,
         attributes: true,
