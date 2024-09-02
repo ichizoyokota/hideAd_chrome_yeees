bb2539e57c5da42cf193229ecbf639682a3b37a8
ichizoyokota <yokota@yeees.in>
2024-09-02 09:49:11 +0900

    0.9.3.1

--- youtube.js
+++ youtube.js
@@ -1,31 +1,14 @@
-localStorage.setItem('back_url_1', location.href);
-if (!localStorage.getItem('back_url_1'))  {
-    localStorage.setItem('back_url_2', location.href);
-}
-
 let observer1 = new MutationObserver((m) => {
 
-    if (localStorage.getItem('yeees_callback_return_flag') === 'true'
-        && location.href === "https://www.youtube.com/shorts"
-        && localStorage.getItem('back_url_2') !== "https://www.youtube.com/shorts") {
-        localStorage.setItem('yeees_callback_return_flag', 'false')
-        history.back()
+    // target
+    //     :
+    //     button#skip-button:3.ytp-skip-ad-button
+
+    let bu = document.querySelectorAll('.ytp-skip-ad-button')
+    if (bu[0]) {
+        bu[0].click()
     }
 
-    m.forEach((r) => {
-        if (r.target.className === "ytp-skip-ad-button") {
-            localStorage.setItem('back_url_2', localStorage.getItem('back_url_1'));
-            localStorage.setItem('back_url_1', location.href);
-            let tmp = document.getElementsByClassName("ytp-skip-ad-button");
-            if (tmp.length > 0) {
-                localStorage.setItem('yeees_callback_return_flag', 'true');
-                if (localStorage.getItem('back_url_2') !== 'https://www.youtube.com/'
-                    || localStorage.getItem('back_url_2') !== 'https://www.youtube.com/shorts') {
-                    location.href = "https://www.youtube.com/shorts"
-                }
-            }
-        }
-    })
 });
 
 let target = document.getElementsByTagName('body');
