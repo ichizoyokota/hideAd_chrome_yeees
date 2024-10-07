83bb7c51553e990460e748d185149748b781f4d8
ichizoyokota <yokota@yeees.in>
2024-10-07 16:29:39 +0900

    0.2.6

--- youtube.js
+++ youtube.js
@@ -1,12 +1,9 @@
 let time_slider = 0
 let time_duration = 0
 let video_id_now_ob = ''
-let video_id_old = ''
-
 let tmp_duration = ''
 let tmp_current = ''
-
-const back_url = 'https://www.youtube.com/watch?v=';
+const back_url = 'https://youtu.be/';
 
 setInterval(() => {
     time_slider++
@@ -29,7 +26,7 @@ const worker_cache_clear = async () => {
     });
 }
 
-const observer1 = new MutationObserver(async (m) => {
+const observer1 = new MutationObserver( async (m) => {
 
     let params_ob = new URL(document.location).searchParams;
 
@@ -40,39 +37,39 @@ const observer1 = new MutationObserver(async (m) => {
         }
 
         if (document.querySelectorAll('.ytp-skip-ad').length > 0
-            || document.querySelectorAll('.ytp-preview-ad').length > 0) {
+            && document.querySelectorAll('.ytp-preview-ad').length > 0) {
             await worker_cache_clear().then(() => {
-                if (time_duration === time_slider) {
-                    location.replace(back_url + video_id_now_ob + '&t=' + (time_slider - 1) + 's')
-                } else if (time_duration > time_slider) {
-                    location.replace(back_url + video_id_now_ob + '&t=' + (time_slider + 1) + 's')
+                if (time_duration > time_slider) {
+                    location.replace(back_url + video_id_now_ob + '?t=' + (time_slider-1) + 's')
+                } else {
+                    location.replace(back_url + video_id_now_ob + '?t=' + (time_duration-1) + 's')
                 }
             })
-        }
-
-        if (document.querySelectorAll('.ytp-time-duration').length > 0) {
-            tmp_duration = document.querySelectorAll('.ytp-time-duration')[0].innerText
-            let duration_obj = tmp_duration.split(':')
-            switch (duration_obj.length) {
-                case 2:
-                    time_duration = (Number(duration_obj[0]) * 60) + Number(duration_obj[1])
-                    break;
-                case 3:
-                    time_duration = (Number(duration_obj[0]) * 60 * 60) + (Number(duration_obj[1]) * 60) + Number(duration_obj[2])
-                    break;
+        } else {
+            if (document.querySelectorAll('.ytp-time-duration').length > 0) {
+                tmp_duration = document.querySelectorAll('.ytp-time-duration')[0].innerText
+                let duration_obj = tmp_duration.split(':')
+                switch (duration_obj.length) {
+                    case 2:
+                        time_duration = (Number(duration_obj[0]) * 60) + Number(duration_obj[1])
+                        break;
+                    case 3:
+                        time_duration = (Number(duration_obj[0]) * 60 * 60) + (Number(duration_obj[1]) * 60) + Number(duration_obj[2])
+                        break;
+                }
             }
-        }
 
-        if (document.querySelectorAll('.ytp-time-current').length > 0) {
-            tmp_current = document.querySelectorAll('.ytp-time-current')[0].innerText
-            let current_obj = tmp_current.split(':')
-            switch (current_obj.length) {
-                case 2:
-                    time_slider = (Number(current_obj[0]) * 60) + Number(current_obj[1])
-                    break;
-                case 3:
-                    time_slider = (Number(current_obj[0]) * 60 * 60) + (Number(current_obj[1]) * 60) + Number(current_obj[2])
-                    break;
+            if (document.querySelectorAll('.ytp-time-current').length > 0) {
+                tmp_current = document.querySelectorAll('.ytp-time-current')[0].innerText
+                let current_obj = tmp_current.split(':')
+                switch (current_obj.length) {
+                    case 2:
+                        time_slider = (Number(current_obj[0]) * 60) + Number(current_obj[1])
+                        break;
+                    case 3:
+                        time_slider = (Number(current_obj[0]) * 60 * 60) + (Number(current_obj[1]) * 60) + Number(current_obj[2])
+                        break;
+                }
             }
         }
     }
