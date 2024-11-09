c866a5a899a227cbd2c867f641908de0fa3d71b8
ichizoyokota <yokota@yeees.in>
2024-11-09 21:21:59 +0900

    0.4.1

--- background.js
+++ background.js
@@ -1,4 +1,3 @@
-
 /* コンテキストメニューがクリックされた時の処理 */
 chrome.contextMenus.onClicked.addListener((info, tab) => {
     switch (info.menuItemId) {
@@ -15,51 +14,51 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
 const updateContextMenus = async (st) => {
     await chrome.contextMenus.create({
         id: "css_status",
-        title: st === 'off' ? chrome.i18n.getMessage('MenuON'): chrome.i18n.getMessage('MenuOFF') ,
+        title: st === 'on' ? chrome.i18n.getMessage('MenuON') : chrome.i18n.getMessage('MenuOFF'),
         contexts: ['all'],
     });
 };
 
 const getCurrentTab = async () => {
-    let queryOptions = { active: true, lastFocusedWindow: true };
-    let [tab] = await chrome.tabs.query(queryOptions);
-    return tab.id;
+    let queryOptions = {active: true, lastFocusedWindow: true};
+    return await chrome.tabs.query(queryOptions);
 }
 
 
-chrome.runtime.onInstalled.addListener(updateContextMenus);
-chrome.runtime.onStartup.addListener(updateContextMenus);
+chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
 
-
-chrome.runtime.onMessage.addListener( async (request, sender, sendResponse) => {
-
-    if (String(request) === 'off') {
-        await chrome.contextMenus.removeAll();
-        await updateContextMenus('off');
-        await chrome.scripting.insertCSS({
-                target : {tabId : await getCurrentTab(), allFrames : true},
-                files : ['adHide.css'],
+    let [tab] = await getCurrentTab()
+    if (tab.url?.startsWith("chrome://") === false) {
+        if (String(request) === 'on') {
+            await chrome.contextMenus.removeAll();
+            await updateContextMenus('on');
+            await chrome.scripting.insertCSS({
+                target: {tabId: tab.id, allFrames: true},
+                files: ['adHide.css'],
             })
-    } else {
-        await chrome.contextMenus.removeAll();
-        await updateContextMenus('on');
-        await chrome.scripting.removeCSS({
-                target : {tabId : await getCurrentTab(), allFrames : true},
-                files : ['adHide.css'],
+        } else {
+            await chrome.contextMenus.removeAll();
+            await updateContextMenus('off');
+            await chrome.scripting.removeCSS({
+                target: {tabId: tab.id, allFrames: true},
+                files: ['adHide.css'],
             })
+        }
     }
 });
 
 
 const css_switch = async () => {
     let css_off_ytp_do_skip = localStorage.getItem('css_off_ytp_do_skip');
-
-    if (css_off_ytp_do_skip !== undefined && css_off_ytp_do_skip !== 'off') {
-        localStorage.setItem('css_off_ytp_do_skip', 'off')
-        chrome.runtime.sendMessage('off')
-
-    } else {
+    if (css_off_ytp_do_skip !== null && css_off_ytp_do_skip !== 'on') {
         localStorage.setItem('css_off_ytp_do_skip', 'on')
         chrome.runtime.sendMessage('on')
+    } else {
+        localStorage.setItem('css_off_ytp_do_skip', 'off')
+        chrome.runtime.sendMessage('off')
     }
 }
+
+chrome.runtime.onInstalled.addListener(async () => {
+    await updateContextMenus()
+});
--- youtube.js
+++ youtube.js
@@ -1,6 +1,8 @@
 let time_slider = 0;
 let time_duration = 0;
-let video_id_now_ob = '';
+let time_slider_before_ad = 0;
+let time_duration_before_ad = 0;
+let time_slider_param = 0;
 let tmp_duration = '';
 let tmp_current = '';
 const back_url = 'https://youtu.be/';
@@ -9,17 +11,50 @@ setInterval(() => {
     time_slider++
 }, 1000);
 
-const observer1 = new MutationObserver(() => {
+const worker_cache_clear = async () => {
+    await navigator.serviceWorker.getRegistrations().then((registrations) => {
+        // 登録されているworkerを全て削除する
+        for (let registration of registrations) {
+            registration.unregister().then(r => null);
+        }
+    });
+    await caches.keys().then((keys) => {
+        // キャッシュストレージを全て削除する
+        keys.forEach((cacheName) => {
+            if (cacheName) {
+                caches.delete(cacheName).then(r => null);
+            }
+        });
+    });
+}
+
+const observer1 = new MutationObserver( async () => {
 
     let params_obj = new URL(document.location).searchParams;
-
     if (params_obj.get("v")) {
-        video_id_now_ob = params_obj.get("v");
+        let video_id_now_ob = String(params_obj.get("v"));
         if (params_obj.get("t")) {
-            time_slider = Number(params_obj.get("t").replace('s', ''));
+             time_slider_param = Number(params_obj.get("t").replace('s', ''));
         }
-
-        if (document.querySelectorAll('.ytp-ad-player-overlay-layout').length === 0) {
+        if (time_slider_before_ad < time_slider_param) {
+            time_slider_before_ad = time_slider_param
+        }
+        if (time_slider_before_ad < time_slider) {
+            time_slider_before_ad = time_slider;
+        }
+        if (time_duration_before_ad <= time_duration) {
+            time_duration_before_ad = time_duration;
+        }
+        if (document.querySelectorAll('.ytp-ad-player-overlay-layout').length > 0) {
+            await observer1.disconnect()
+            await worker_cache_clear().then(() => {
+                if (time_duration_before_ad <= time_slider_before_ad) {
+                    location.replace(back_url + video_id_now_ob + '?t=' + (time_duration_before_ad - 3) + 's');
+                } else {
+                    location.reload();
+                }
+            })
+        } else {
             tmp_duration = document.querySelectorAll('.ytp-time-duration')[0].innerText
             let duration_obj = tmp_duration.split(':')
             switch (duration_obj.length) {
@@ -30,7 +65,6 @@ const observer1 = new MutationObserver(() => {
                     time_duration = (Number(duration_obj[0]) * 60 * 60) + (Number(duration_obj[1]) * 60) + Number(duration_obj[2]);
                     break;
             }
-
             tmp_current = document.querySelectorAll('.ytp-time-current')[0].innerText;
             let current_obj = tmp_current.split(':');
             switch (current_obj.length) {
@@ -41,21 +75,9 @@ const observer1 = new MutationObserver(() => {
                     time_slider = (Number(current_obj[0]) * 60 * 60) + (Number(current_obj[1]) * 60) + Number(current_obj[2]);
                     break;
             }
-
-        } else {
-            // observer1.disconnect()
-            setTimeout(()=> {
-                if (time_duration > time_slider) {
-                    location.replace(back_url + video_id_now_ob + '?t=' + time_slider + 's');
-                } else if (time_duration < time_slider) {
-                    location.replace(back_url + video_id_now_ob + '?t=' + (time_duration -1) + 's');
-                } else {
-                    location.reload();
-                }
-            }, 2000);
         }
     }
-});
+})
 
 observer1.observe(document.getElementsByTagName('body')[0], {
     childList: true,
@@ -65,6 +87,11 @@ observer1.observe(document.getElementsByTagName('body')[0], {
 });
 
 
-window.addEventListener("beforeunload", (event) => {
-    localStorage.removeItem('css_off_ytp_do_skip');
+window.addEventListener("load", () => {
+    let css_off_ytp_do_skip = localStorage.getItem('css_off_ytp_do_skip');
+    if (css_off_ytp_do_skip !== null && css_off_ytp_do_skip === 'off') {
+        chrome.runtime.sendMessage('off')
+    } else if (css_off_ytp_do_skip !== null && css_off_ytp_do_skip === 'on') {
+        chrome.runtime.sendMessage('on')
+    }
 })
